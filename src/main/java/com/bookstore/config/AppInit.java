84c4dd314ec427d74cb7812860277b8b845d1e97
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-04-23 08:36:43 +0700

    appinit + webconfig

--- src/main/java/com/bookstore/config/AppInit.java
+++ src/main/java/com/bookstore/config/AppInit.java
@@ -1,4 +1,33 @@
 package com.bookstore.config;
 
-public class AppInit {
+import jakarta.servlet.Filter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter @Nullable [] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return new Filter[]{filter};
+    }
 }
--- src/main/java/com/bookstore/config/WebConfig.java
+++ src/main/java/com/bookstore/config/WebConfig.java
@@ -1,4 +1,60 @@
 package com.bookstore.config;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import org.thymeleaf.Thymeleaf;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.bookstore")
 public class WebConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    // 1. template resolver
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+
+        resolver.setApplicationContext(applicationContext);
+
+        resolver.setPrefix("/webapp/WEB-INF/templates");
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding("UTF-8");
+
+        return resolver;
+    }
+
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+
+        engine.setTemplateResolver(templateResolver());
+
+        // important
+        engine.addDialect(new LayoutDialect());
+        return engine;
+    }
+
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+
+        return resolver;
+    }
+
+
+
+
 }
